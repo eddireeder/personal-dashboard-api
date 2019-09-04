319b6ef5f1c8b81bb57b55e1559ef83f8645249e
Eddie Reeder <e.reeder1997@gmail.com>
2019-09-04 15:55:15 +0100

    Added logging

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+const morgan = require("morgan");
 const express = require('express');
 const app = express();
 const port = 3000;
+
+// Log all requests
+app.use(morgan('[:date[clf]] :method :url :status - :response-time ms'));
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
