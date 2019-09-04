efd51d4d6a3a3b06c4e27df041668e71017db6c0
Eddie Reeder <e.reeder1997@gmail.com>
2019-09-04 16:11:31 +0100

    Added route structure and fixed environment variable port bug

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
-const morgan = require("morgan");
-const bodyParser = require("body-parser");
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
 const app = express();
+
+// Read .env file
+dotenv.config();
 
 // Parse incoming request bodies as JSON
 app.use(bodyParser.json());
@@ -9,7 +13,8 @@ app.use(bodyParser.json());
 // Log all requests
 app.use(morgan('[:date[clf]] :method :url :status - :response-time ms'));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+// Use defined routes
+app.use('/', require('./routes'));
 
 // Handle route not found
 app.use((req, res, next) => {
@@ -23,6 +28,6 @@ app.use((err, req, res, next) => {
 });
 
 // Start listening
-app.listen(process.env.port, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server running on port " + process.env.PORT);
 });
