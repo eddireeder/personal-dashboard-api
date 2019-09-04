be784304bf4279d8327f53381fe7be1e555184c9
Eddie Reeder <e.reeder1997@gmail.com>
2019-09-04 15:59:08 +0100

    Created error handlers for 404 and 500

--- server.js
+++ server.js
@@ -12,4 +12,15 @@ app.use(morgan('[:date[clf]] :method :url :status - :response-time ms'));
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
+// Handle route not found
+app.use((req, res, next) => {
+  return res.sendStatus(404);
+});
+
+// Handle server errors
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  return res.status(500).json({error: "Something broke"})
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
