86b684733e044d81852150924ca7f16cf29b1f29
Eddie Reeder <e.reeder1997@gmail.com>
2019-09-05 21:07:22 +0100

    Added rough authentication

--- src/index.js
+++ src/index.js
@@ -3,25 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import {Route, BrowserRouter as Router, Switch, Redirect} from 'react-router-dom'
 import App from './components/App/App';
-import Authentication from './components/Authentication/Authentication';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
 import NotFound from './components/NotFound/NotFound';
 import * as serviceWorker from './serviceWorker';
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route {...rest} render = {
-    props => (localStorage.getItem('user') ? <Component {...props} /> : <Redirect to = {{pathname: '/auth/login', state: {from: props.location}}}/>)
+    props => (localStorage.getItem('user') ? <Component {...props} /> : <Redirect to = {{pathname: '/login', state: {from: props.location}}}/>)
   }/>
 );
 
 const routing = (
   <Router>
-    <div>
-      <Switch>
-        <Route exact path="/auth" component={Authentication}/>
-        <PrivateRoute exact path="/" component={App}/>
-        <Route component={NotFound}/>
-      </Switch>
-    </div>
+    <Switch>
+      <Route path="/login" component={Login}/>
+      <Route path="/register" component={Register}/>
+      <PrivateRoute exact path="/" component={App}/>
+      <Route component={NotFound}/>
+    </Switch>
   </Router>
 )
 
